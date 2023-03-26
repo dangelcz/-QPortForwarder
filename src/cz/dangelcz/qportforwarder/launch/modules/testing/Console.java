d6387a50d7f7f7e32aab2c2799993c3abec86653
Daniel Pavelek <danielpavelek@seznam.cz>
2023-03-26 15:56:06 +0200

    Console module moved into testing package

---
+++ src/cz/dangelcz/qportforwarder/launch/modules/testing/Console.java
@@ -0,0 +1,65 @@
+package cz.dangelcz.qportforwarder.launch.modules.testing;
+
+import cz.dangelcz.qportforwarder.launch.ARunModule;
+import cz.dangelcz.qportforwarder.launch.AppLauncher;
+import cz.dangelcz.qportforwarder.launch.ArgumentReader;
+import cz.dangelcz.qportforwarder.launch.annotation.RunModule;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+
+@RunModule
+public class Console extends ARunModule
+{
+	@Override
+	public void run(ArgumentReader arguments)
+	{
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		
+		while (true)
+		{
+			try
+			{
+				System.out.println("\n-----------------------");
+				System.out.println("Run your command (or 'exit'):");
+				System.out.print("> ");
+				
+				// String command = System.console().readLine();
+				String command = sc.nextLine();
+				
+				if ("exit".equalsIgnoreCase(command))
+				{
+					return;
+				}
+				
+				AppLauncher.main(command.split(" "));
+			} catch (NoSuchElementException e)
+			{
+				return;
+			} catch (Exception e)
+			{
+				System.err.println("You got exception, but console runs again!");
+			}
+		}
+	}
+
+	@Override
+	public String getHelpDescription()
+	{
+		return "developer tool - reruns given command";
+	}
+
+	@Override
+	public String getRequiredParameters()
+	{
+		return "any";
+	}
+
+	@Override
+	public String getOptionalParameters()
+	{
+		return "any";
+	}
+}
