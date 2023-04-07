35e84dfd925268be3076a799528e42cdeb4328b9
Daniel Pavelek <danielpavelek@seznam.cz>
2023-04-07 09:28:42 +0200

    Dark theme switch

--- src/cz/dangelcz/qportforwarder/gui/controllers/ForwardingPaneController.java
+++ src/cz/dangelcz/qportforwarder/gui/controllers/ForwardingPaneController.java
@@ -2,12 +2,15 @@ package cz.dangelcz.qportforwarder.gui.controllers;
 
 import cz.dangelcz.qportforwarder.config.AppConfig;
 import cz.dangelcz.qportforwarder.gui.WindowApplication;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -19,10 +22,14 @@ public class ForwardingPaneController implements Initializable
 	@FXML
 	private VBox lineContainer;
 
+	@FXML
+	private BorderPane rootPane;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
+		toggleDarkMode();
+
 		for (int i = 0; i < AppConfig.DEFAULT_LINE_COUNT; i++)
 		{
 			addLinePanel();
@@ -50,5 +57,27 @@ public class ForwardingPaneController implements Initializable
 	public void onAddButtonClick()
 	{
 		addLinePanel();
+	}
+
+	private void toggleDarkMode()
+	{
+		String cssFile = "/dark_theme.css";
+
+		ObservableList<String> stylesheets = rootPane.getStylesheets();
+
+		if (stylesheets.contains(cssFile))
+		{
+			stylesheets.remove(cssFile);
+		}
+		else
+		{
+			stylesheets.add(cssFile);
+		}
+
+	}
+
+	public void onDarkModeClick(ActionEvent actionEvent)
+	{
+		toggleDarkMode();
 	}
 }
